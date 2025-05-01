8c24268fac823c3e6be0a0e878fef98c05e70e07
ainnot <stkim2007@gmail.com>
2025-05-01 14:29:39 +0900

    pie

--- src/screens/Tab/Analyze/index.tsx
+++ src/screens/Tab/Analyze/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Image, ScrollView, Text, View} from 'react-native';
-import {BarChart} from 'react-native-gifted-charts';
+import {BarChart, PieChart} from 'react-native-gifted-charts';
 
 import FireSvg from '@/assets/images/fire.svg';
 import {useAuth} from '@/contexts/AuthContext';
@@ -18,6 +18,10 @@ const Analyze = () => {
     ],
     label: ['월', '화', '수', '목', '금', '토', '일'][i],
   }));
+  const pieData = [
+    {value: 55, color: '#EE902C'},
+    {value: 45, color: '#344BFD'},
+  ];
 
   return (
     <ScrollView contentContainerStyle={{alignItems: 'center', padding: 26}}>
@@ -29,6 +33,57 @@ const Analyze = () => {
             <View style={{flexDirection: 'row', alignItems: 'center', gap: 4}}>
               <Text style={[typography.body, {color: theme.red, fontWeight: '600'}]}>평균 집중도 78%</Text>
               <FireSvg width={16} height={16} />
+            </View>
+          </View>
+        </View>
+
+        <View style={{gap: 12}}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+            <FontAwesome6 name="angle-left" size={22} color={theme.text} iconStyle="solid" />
+            <View style={{flexDirection: 'column', alignItems: 'center', gap: 4}}>
+              <Text style={[typography.subtitle, {color: theme.text, fontWeight: 600}]}>일간 공부 시간</Text>
+              <Text style={[typography.body, {color: theme.text}]}>2025. 05. 01</Text>
+            </View>
+            <FontAwesome6 name="angle-right" size={22} color={theme.text} iconStyle="solid" />
+          </View>
+          <View style={{backgroundColor: '#ffffff', borderRadius: 12, paddingHorizontal: 24, paddingVertical: 18, gap: 8}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+              <View>
+                <Text style={[typography.body, {color: theme.text}]}>오늘 공부</Text>
+                <Text style={[typography.title, {color: theme.text}]}>3시간 50분</Text>
+              </View>
+              <View style={{alignItems: 'flex-end'}}>
+                <Text style={[typography.body, {color: theme.text}]}>어제보다</Text>
+                <Text style={[typography.body, {color: theme.primary, fontWeight: 500}]}>+30분</Text>
+              </View>
+            </View>
+
+            <View style={{alignItems: 'center', marginTop: 18, marginBottom: 18}}>
+              <PieChart donut innerRadius={62} data={pieData} />
+            </View>
+
+            <View style={{gap: 4, marginTop: 16}}>
+              <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 8}}>
+                  <View style={{width: 15, height: 15, borderColor: '#344BFD', borderWidth: 3, borderRadius: 15 / 2}} />
+                  <Text style={[typography.body, {color: theme.secondary, fontWeight: 600}]}>순 공부시간</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 16}}>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>1시간 20분</Text>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>55%</Text>
+                </View>
+              </View>
+
+              <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 8}}>
+                  <View style={{width: 15, height: 15, borderColor: '#EE902C', borderWidth: 3, borderRadius: 15 / 2}} />
+                  <Text style={[typography.body, {color: theme.secondary, fontWeight: 600}]}>공부 이외 시간</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 16}}>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>1시간</Text>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>45%</Text>
+                </View>
+              </View>
             </View>
           </View>
         </View>
@@ -54,61 +109,63 @@ const Analyze = () => {
               </View>
             </View>
 
-            <BarChart
-              stackData={stackData.map(item => ({
-                ...item,
-                stacks: item.stacks.map((stack, index) => {
-                  if (index === 0) {
-                    return {...stack, borderRadius: 0};
-                  }
-                  if (index === 1) {
-                    // return {...stack, marginBottom: 1};
-                  }
-                  return stack;
-                }),
-              }))}
-              barBorderTopLeftRadius={12}
-              barBorderTopRightRadius={12}
-              barWidth={18}
-              hideRules
-              rulesType="solid"
-              rulesColor={theme.inactive}
-              noOfSections={3}
-              // disablePress
-              // hideYAxisText
-              // yAxisLabelSuffix="분"
-              yAxisThickness={0}
-              xAxisThickness={0}
-              // showValuesAsTopLabel
-              xAxisColor={theme.inactive}
-              xAxisLabelTextStyle={{color: theme.text}}
-              disableScroll
-              isAnimated
-              animationDuration={200}
-              yAxisExtraHeight={32}
-              autoCenterTooltip
-              renderTooltip={(item: any) => {
-                return (
-                  <View
-                    style={{
-                      backgroundColor: theme.background,
-                      paddingHorizontal: 8,
-                      paddingVertical: 4,
-                      borderRadius: 8,
-                      borderColor: theme.primary,
-                    }}>
-                    <Text
+            <View style={{alignItems: 'center'}}>
+              <BarChart
+                stackData={stackData.map(item => ({
+                  ...item,
+                  stacks: item.stacks.map((stack, index) => {
+                    if (index === 0) {
+                      return {...stack, borderRadius: 0};
+                    }
+                    if (index === 1) {
+                      // return {...stack, marginBottom: 1};
+                    }
+                    return stack;
+                  }),
+                }))}
+                barBorderTopLeftRadius={12}
+                barBorderTopRightRadius={12}
+                barWidth={18}
+                hideRules
+                rulesType="solid"
+                rulesColor={theme.inactive}
+                noOfSections={3}
+                // disablePress
+                hideYAxisText
+                // yAxisLabelSuffix="분"
+                yAxisThickness={0}
+                xAxisThickness={0}
+                // showValuesAsTopLabel
+                xAxisColor={theme.inactive}
+                xAxisLabelTextStyle={{color: theme.text}}
+                disableScroll
+                isAnimated
+                animationDuration={200}
+                yAxisExtraHeight={32}
+                autoCenterTooltip
+                renderTooltip={(item: any) => {
+                  return (
+                    <View
                       style={{
-                        color: theme.text,
-                        fontSize: 12,
-                        fontWeight: '500',
+                        backgroundColor: theme.background,
+                        paddingHorizontal: 8,
+                        paddingVertical: 4,
+                        borderRadius: 8,
+                        borderColor: theme.primary,
                       }}>
-                      {item.stacks.map((stack: any) => stack.value).reduce((a: number, b: number) => a + b, 0)} 분
-                    </Text>
-                  </View>
-                );
-              }}
-            />
+                      <Text
+                        style={{
+                          color: theme.text,
+                          fontSize: 12,
+                          fontWeight: '500',
+                        }}>
+                        {item.stacks.map((stack: any) => stack.value).reduce((a: number, b: number) => a + b, 0)} 분
+                      </Text>
+                    </View>
+                  );
+                }}
+              />
+            </View>
 
             <View style={{gap: 4, marginTop: 16}}>
               <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
