ec84729937b839f89796f532cc3884869721a9e5
ainnot <stkim2007@gmail.com>
2025-05-02 10:20:23 +0900

    add weeklygoal chart
    and etc

--- src/screens/Tab/Analyze/index.tsx
+++ src/screens/Tab/Analyze/index.tsx
@@ -18,6 +18,18 @@ const Analyze = () => {
     ],
     label: ['월', '화', '수', '목', '금', '토', '일'][i],
   }));
+  const weeklyGoalData = Array.from({length: 7}, (_, i) => {
+    const stackValue1 = Math.floor(Math.random() * 50) + 10; // 첫 번째 stack value
+    const stackValue2 = Math.floor(Math.random() * 50) + 10; // 두 번째 stack value
+    return {
+      stacks: [
+        {value: stackValue1, color: '#ff7171'}, // 첫 번째 stack 값
+        {value: Math.max(stackValue2 - stackValue1, 0), color: '#C6CED1'}, // 두 번째 stack 값
+      ],
+      label: ['월', '화', '수', '목', '금', '토', '일'][i], // 요일 값
+    };
+  });
+
   const pieData = [
     {value: 55, color: '#EE902C'},
     {value: 45, color: '#344BFD'},
@@ -58,7 +70,7 @@ const Analyze = () => {
               </View>
             </View>
 
-            <View style={{alignItems: 'center', marginTop: 18, marginBottom: 18}}>
+            <View style={{alignItems: 'center', gap: 18}}>
               <PieChart donut innerRadius={62} data={pieData} />
             </View>
 
@@ -192,85 +204,115 @@ const Analyze = () => {
             </View>
           </View>
         </View>
+
+        <View style={{gap: 12}}>
+          <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
+            <FontAwesome6 name="angle-left" size={22} color={theme.text} iconStyle="solid" />
+            <View style={{flexDirection: 'column', alignItems: 'center', gap: 4}}>
+              <Text style={[typography.subtitle, {color: theme.text, fontWeight: 600}]}>주간 목표 달성률</Text>
+              <Text style={[typography.body, {color: theme.text}]}>2025. 03. 31 ~ 2025. 04. 07</Text>
+            </View>
+            <FontAwesome6 name="angle-right" size={22} color={theme.text} iconStyle="solid" />
+          </View>
+          <View style={{backgroundColor: '#ffffff', borderRadius: 12, paddingHorizontal: 24, paddingVertical: 18, gap: 8}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+              <View>
+                <Text style={[typography.body, {color: theme.text}]}>이번주 평균 달성률</Text>
+                <Text style={[typography.title, {color: theme.text}]}>95%</Text>
+              </View>
+              <View style={{alignItems: 'flex-end'}}>
+                <Text style={[typography.body, {color: theme.text}]}>지난주보다</Text>
+                <Text style={[typography.body, {color: '#ff7171', fontWeight: 500}]}>+2.5%</Text>
+              </View>
+            </View>
+
+            <View style={{alignItems: 'center'}}>
+              <BarChart
+                stackData={weeklyGoalData.map(item => ({
+                  ...item,
+                  stacks: item.stacks.map((stack, index) => {
+                    if (index === 0) {
+                      if (item.stacks[1].value) {
+                        return {...stack, borderRadius: 0};
+                      }
+                    }
+                    if (index === 1) {
+                      // return {...stack, marginBottom: 1};
+                    }
+                    return stack;
+                  }),
+                }))}
+                barBorderTopLeftRadius={12}
+                barBorderTopRightRadius={12}
+                barWidth={18}
+                hideRules
+                rulesType="solid"
+                rulesColor={theme.inactive}
+                noOfSections={3}
+                // disablePress
+                hideYAxisText
+                // yAxisLabelSuffix="분"
+                yAxisThickness={0}
+                xAxisThickness={0}
+                // showValuesAsTopLabel
+                xAxisColor={theme.inactive}
+                xAxisLabelTextStyle={{color: theme.text}}
+                disableScroll
+                isAnimated
+                animationDuration={200}
+                yAxisExtraHeight={32}
+                autoCenterTooltip
+                renderTooltip={(item: any) => {
+                  return (
+                    <View
+                      style={{
+                        backgroundColor: theme.background,
+                        paddingHorizontal: 8,
+                        paddingVertical: 4,
+                        borderRadius: 8,
+                        borderColor: theme.primary,
+                      }}>
+                      <Text
+                        style={{
+                          color: theme.text,
+                          fontSize: 12,
+                          fontWeight: '500',
+                        }}>
+                        {item.stacks.map((stack: any) => stack.value).reduce((a: number, b: number) => a + b, 0)} 분
+                      </Text>
+                    </View>
+                  );
+                }}
+              />
+            </View>
+
+            <View style={{gap: 4, marginTop: 16}}>
+              <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 8}}>
+                  <View style={{width: 15, height: 15, borderColor: '#344BFD', borderWidth: 3, borderRadius: 15 / 2}} />
+                  <Text style={[typography.body, {color: theme.secondary, fontWeight: 600}]}>순 공부시간</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 16}}>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>9시간 14분</Text>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>55%</Text>
+                </View>
+              </View>
+
+              <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 8}}>
+                  <View style={{width: 15, height: 15, borderColor: '#EE902C', borderWidth: 3, borderRadius: 15 / 2}} />
+                  <Text style={[typography.body, {color: theme.secondary, fontWeight: 600}]}>공부 이외 시간</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center', gap: 16}}>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>3시간 50분</Text>
+                  <Text style={[typography.body, {color: theme.text, fontWeight: 400}]}>45%</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
 };
-
-/* <View style={{flexDirection: 'column'}}>
-        <View style={{flexDirection: 'row'}}>
-          <View></View>
-          <View style={{flexDirection: 'column'}}>
-            <Text>일간 공부 시간</Text>
-            <Text>대충 날짜</Text>
-          </View>
-          <View></View>
-        </View>
-        <View style={{flexDirection: 'column'}}>
-          <View>
-            <View style={{flexDirection: 'column'}}>
-              <Text>오늘 공부</Text>
-              <Text>대충 공부시간</Text>
-            </View>
-            <Text>어제보다 ~분</Text>
-          </View>
-          <View>
-            <Text>그래프</Text>
-          </View>
-          <View style={{flexDirection: 'column'}}>
-            <View>
-              <View></View>
-              <Text>순 공부 시간</Text>
-              <Text>대충 순 공부시간</Text>
-              <Text>대충 순 공부시간 퍼센트</Text>
-            </View>
-            <View>
-              <View></View>
-              <Text>공부 이외의 시간</Text>
-              <Text>대충 공부 이외의 시간시간</Text>
-              <Text>대충 공부 이외의 시간 퍼센트</Text>
-            </View>
-          </View>
-        </View>
-      </View>
-
-      <View style={{flexDirection: 'column'}}>
-        <View style={{flexDirection: 'row'}}>
-          <View></View>
-          <View style={{flexDirection: 'column'}}>
-            <Text>주간 공부 시간</Text>
-            <Text>대충 날짜 ~ 대충 날짜</Text>
-          </View>
-          <View></View>
-        </View>
-        <View style={{flexDirection: 'column'}}>
-          <View>
-            <View style={{flexDirection: 'column'}}>
-              <Text>이번주 평균</Text>
-              <Text>대충 이번주 공부시간</Text>
-            </View>
-            <Text>지난주보다 ~분</Text>
-          </View>
-          <View>
-            <Text>그래프</Text>
-          </View>
-          <View style={{flexDirection: 'column'}}>
-            <View>
-              <View></View>
-              <Text>순 공부 시간</Text>
-              <Text>대충 순 공부시간</Text>
-              <Text>대충 순 공부시간 퍼센트</Text>
-            </View>
-            <View>
-              <View></View>
-              <Text>공부 이외의 시간</Text>
-              <Text>대충 공부 이외의 시간시간</Text>
-              <Text>대충 공부 이외의 시간 퍼센트</Text>
-            </View>
-          </View>
-        </View>
-      </View>
-
-      <View></View> */
-
 export default Analyze;
